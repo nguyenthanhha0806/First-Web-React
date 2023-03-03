6106f54b10c51500b702afeba243eb40f015351f
nguyenthanhha0806 <riverandsnow300418@gmail.com>
2023-03-03 10:37:28 +0700

    Create Defaut Layout for project

--- src/Components/Layout/DefaultLayout/index.js
+++ src/Components/Layout/DefaultLayout/index.js
@@ -1,12 +1,16 @@
+import classNames from 'classnames/bind'
+import styles from './DefaultLayout.module.scss'
 import Header from './Header'
 import Sidebar from './Sidebar'
+
+const cx = classNames.bind(styles)
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header></Header>
-            <div className='container'>
+            <div className={cx('container')}>
                 <Sidebar></Sidebar>
-                <div className='content'>
+                <div className={cx('content')}>
                     {children}
                 </div>
             </div>
